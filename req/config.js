2f08d88bfb50fc5dd38ba1c79c6525b84e376cf1
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2022-02-11 18:29:43 +0700

    update config

--- req/config.js
+++ req/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://streamingin.elcodee.com/v1/api";
-const baseURL = "http://127.0.0.1:8000/api";
+const baseURL = "https://streamingin.elcodee.com/v1/api";
+// const baseURL = "http://127.0.0.1:8000/api";
 
 export const API = axios.create({
   baseURL: baseURL,
